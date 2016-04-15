46c0b7503f8254ff6124e2816459e2b85e1acb48
Sagar Mutha <sagar2305@gmail.com>
2016-04-14 17:58:35 -0700

    demo

--- iBeacon/AppDelegate.swift
+++ iBeacon/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert , .Sound, .Badge], categories: nil))
         return true
     }
 
--- iBeacon/ViewController.swift
+++ iBeacon/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "416C0120-5960-4280-A67C-A2A9BB166D0F")!, identifier: "SmartApps_Beacon")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() != .AuthorizedAlways {
+            locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
+        locationManager.startMonitoringForRegion(region)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sendLocalNotificationWithMessage(message: String) {
+        let notification = UILocalNotification()
+        
+        notification.alertBody = message
+        notification.alertAction = "View Details"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        
+        UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+    }
 
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        print(beacons)
+    }
 
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("You have entered the beacon region")
+        sendLocalNotificationWithMessage("You have entered the beacon region")
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("You have exited the beacon region")
+        sendLocalNotificationWithMessage("You have exited the beacon region")
+    }
 }
 
